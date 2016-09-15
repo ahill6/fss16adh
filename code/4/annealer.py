04e20e191e6167db49c17307de029057f894c4f0
Andrew Hill <ahill6@ncsu.edu>
2016-09-15 18:56:13 +0000

    Final SA commit

--- code/4/annealer.py
+++ code/4/annealer.py
@@ -1,53 +1,51 @@
 from __future__ import print_function
-import random
+import random, sys
 from math import exp
-import sys
 
 def schaffer(x):
     return x**2 + (x-2)**2
     
-def energy(x):
-    min = 2.0
-    max = 19210.0
-    return (x - min) / (max - min)
+def energy(f, x):
+    return f(x) # scaling is not needed for the optimizer, but for the heat/probability calculation
     
 def neighbor():
-    return random.randint(-10000,10000)
+    return random.randint(-100, 100) # This value was semi-arbitrary, as chosen because it is 1% of domain
 
 def output(x): print(x, end="")
 
 def P(current, next, heat):
-    max = 199960004.0
+    max = 199960004.0 
     min = 2.0
     old = (current - min) / (max - min)
     new = (next - min) / (max - min)
+    
     if heat != 0:
         return exp((old-new)/heat)
-    """
-    if heat != 0:
-        return exp((current-next)/heat)
     else:
         print("heat = 0, divide by zero")
         print (current, next, heat)
         sys.exit()
-    """
+        
+def move(cur):
+    return cur + neighbor()
     
 def run():
     kmax = 1000.0
-    emin = 2
+    emin = 3
+    opt_func = schaffer
     current_state = random.randint(-10000,10000)
-    #current_energy = energy(schaffer(current_state))
-    current_energy = schaffer(current_state)
+    current_energy = energy(opt_func, current_state)
     best_state = current_state
     best_energy = current_energy
     k = 1
     
     # Run Simulated Annealer
     while (k < kmax and current_energy > emin):
-        next_state = neighbor()
-        #next_energy = energy(schaffer(next_state))
-        next_energy = schaffer(next_state)
+        next_state = move(current_state)
+        next_energy = energy(opt_func, next_state)
         if next_energy < best_energy:
+            current_state = next_state
+            current_energy = next_energy
             best_state = next_state
             best_energy = next_energy
             output("!")
@@ -64,27 +62,8 @@ def run():
         k += 1
         if k%25 == 0:
             print("\t", best_state, best_energy)
-    print("END")
-    print(best_state)
-    print(best_energy)
+            
+    print("\nEnded at", k)
+    print("state \t", best_state)
+    print("energy\t", best_energy)
     #return best_state
-            
-            
-def PCheck():
-    kmax = 1000.0
-    emin = 2
-    current_state = random.randint(-10000,10000)
-    #current_energy = energy(schaffer(current_state))
-    current_energy = schaffer(current_state)
-    best_state = current_state
-    best_energy = current_energy
-    k = 1
-    
-    # Run Simulated Annealer
-    while (k < kmax and current_energy > emin):
-        next_state = neighbor()
-        #next_energy = energy(schaffer(next_state))
-        next_energy = schaffer(next_state)
-        P(current_energy, next_energy, k/kmax)
-        k += 1
-
--- code/4/main.py
+++ code/4/main.py
@@ -1,17 +1,3 @@
-from schaffer_sa import *
+from annealer import *
 
 run()
-#PCheck()
-"""
-max = 0
-min = 100
-for x in range(10000):
-    a = schaffer(x)
-    if a > max:
-        max = a
-    if a < min:
-        min = a
-        
-print min
-print max
-"""
